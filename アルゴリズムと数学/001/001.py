931b55f85f050f37cc0bafd77204de258a6bb661
natyosu3 <nachi334825@gmail.com>
2023-06-15 23:10:33 +0900

    add

--- アルゴリズムと数学/001/001.py
+++ アルゴリズムと数学/001/001.py
@@ -1,16 +1,4 @@
 # Print 5+N
 
-def main():
-    while True:
-        try:
-            a1, a2, a3 = map(int, input().split())
-            if 1 <= a1 <= 100 and 1 <= a2 <= 100 and 1 <= a3 <= 100:
-                break
-        except:
-            pass
-
-    total_sum = a1 + a2 + a3
-    print(total_sum)
-
-if __name__ == "__main__":
-    main()
+n=int(input())
+print(5+n)
